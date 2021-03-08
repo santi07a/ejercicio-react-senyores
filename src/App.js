22d15a012f54c0fcd7d8b6d78fe57712d9455394
Santi Arnau <santi07a@gmail.com>
2021-03-08 16:30:59 +0100

    agregado header html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+
+import "./index.css";
 
 function App() {
+
+
   return (
     <>
-
+      <header className="cabecera">
+        <h1>Señores que te apuntan con el dedo</h1>
+        <p className="totales"><span className="nmarcados">0</span> señores que te apuntan con el dedo marcados</p>
+        <a className="accion-marcar" href="marcar-todos">Marcar todos</a>
+      </header>
     </>
   );
 }
